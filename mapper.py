3b2b70f24775cae79fc4f6dd37b0e8157d490c51
Jakub Kadlubiec <jakub.kadlubiec@skyscanner.net>
2017-06-10 01:04:08 +0100

    Refactor mapper

--- mapper.py
+++ mapper.py
@@ -1,64 +1,52 @@
 import csv
 
-input_lloyds = []
 
-with open('lloyds.csv', newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        input_lloyds.append(row)
-
-# Transaction Date,Transaction Type,Sort Code,Account Number,Transaction Description,Debit Amount,Credit Amount,Balance,
-# Transaction Date,Transaction Type,Sort Code,Account Number,Transaction Description,Debit Amount,Credit Amount,Balance,
-# Date, Account,  Amount GBP ,  Amount CZK , Category, Sub-category, Note, Merchant, Holiday, Business
-
-output = []
-
-for input_row in input_lloyds[1:]:
-  credit = 0 if input_row[6] == '' else float(input_row[6])
-  debit = 0 if input_row[5] == '' else float(input_row[5])
-
-  output_row = [
-    input_row[0],
-    'LloydsMaja',
-    credit - debit,
-    '',
-    '',
-    '',
-    input_row[4],
-    input_row[4],
-    '',
-    '',
-  ]
-  output.append(output_row)
+def read_bank_statement(file_name):
+    input = []
+    with open(file_name, newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            input.append(row)
+    return input
 
 
-input_tsb = []
+def map_lloyds_transactions(transactions, account_name):
+    output = []
+    for input_row in transactions[1:]:
+        credit = 0 if input_row[6] == '' else float(input_row[6])
+        debit = 0 if input_row[5] == '' else float(input_row[5])
 
-with open('tsb.csv', newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        input_tsb.append(row)
+        output_row = [
+            input_row[0],
+            account_name,
+            credit - debit,
+            '',
+            '',
+            '',
+            input_row[4],
+            input_row[4],
+            '',
+            '',
+        ]
+        output.append(output_row)
 
-for input_row in input_tsb[1:]:
-  credit = 0 if input_row[6] == '' else float(input_row[6])
-  debit = 0 if input_row[5] == '' else float(input_row[5])
+    return output
 
-  output_row = [
-    input_row[0],
-    'TSBJakub',
-    credit - debit,
-    '',
-    '',
-    '',
-    input_row[4],
-    input_row[4],
-    '',
-    '',
-  ]
-  output.append(output_row)
 
-with open('output.csv', 'w', newline='') as f:
-    f.write('Date\tAccount\tAmount GBP\tAmount CZK\tCategory\tSub-category\tNote\tMerchant\tHoliday\tBusiness\n')
+def write_output(output):
+    with open('output.csv', 'w', newline='') as f:
+        f.write('Date\tAccount\tAmount GBP\tAmount CZK\tCategory\tSub-category\tNote\tMerchant\tHoliday\tBusiness\n')
 
-    writer = csv.writer(f, delimiter='\t')
-    writer.writerows(output)
+        writer = csv.writer(f, delimiter='\t')
+        writer.writerows(output)
+
+
+lloyds_transactions = read_bank_statement('lloyds.csv')
+output_lloyds = map_lloyds_transactions(lloyds_transactions, 'LloydsMaja')
+
+tsb_transactions = read_bank_statement('tsb.csv')
+output_tsb = map_lloyds_transactions(tsb_transactions, 'TSBJakub')
+
+output = output_lloyds + output_tsb
+
+write_output(output)

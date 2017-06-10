a2c36ef41d1652289309fc8a7004762b6c5a41b5
Jakub Kadlubiec <jakub.kadlubiec@skyscanner.net>
2017-06-10 12:39:52 +0100

    Add monzo mapping

--- mapper.py
+++ mapper.py
@@ -1,4 +1,5 @@
 import csv
+import datetime
 
 
 def read_bank_statement(file_name):
@@ -17,16 +18,39 @@ def map_lloyds_transactions(transactions, account_name):
         debit = 0 if input_row[5] == '' else float(input_row[5])
 
         output_row = [
-            input_row[0],
-            account_name,
-            credit - debit,
-            '',
-            '',
-            '',
-            input_row[4],
-            input_row[4],
-            '',
-            '',
+            input_row[0],  # Date
+            account_name,  # Account
+            credit - debit,  # Amount GBP
+            '',  # Amount CZK
+            '',  # Category
+            '',  # Sub-category
+            input_row[4],  # Note
+            input_row[4],  # Merchant
+            '',  # Holiday
+            '',  # Business
+        ]
+        output.append(output_row)
+
+    return output
+
+
+def map_monzo_transactions(transactions, account_name):
+    output = []
+
+    for input_row in transactions[1:]:
+        transaction_datetime = datetime.datetime.strptime(input_row[1], '%Y-%m-%d %H:%M:%S %z')
+
+        output_row = [
+            transaction_datetime.strftime('%d/%m/%Y'),  # Date
+            account_name,  # Account
+            float(input_row[2]),  # Amount GBP
+            '',  # Amount CZK
+            '',  # Category
+            '',  # Sub-category
+            input_row[8],  # Note
+            input_row[8],  # Merchant
+            '',  # Holiday
+            '',  # Business
         ]
         output.append(output_row)
 
@@ -47,6 +71,9 @@ output_lloyds = map_lloyds_transactions(lloyds_transactions, 'LloydsMaja')
 tsb_transactions = read_bank_statement('tsb.csv')
 output_tsb = map_lloyds_transactions(tsb_transactions, 'TSBJakub')
 
-output = output_lloyds + output_tsb
+monzo_transactions = read_bank_statement('monzo.csv')
+output_monzo = map_monzo_transactions(monzo_transactions, 'MonzoMaja')
+
+output = output_lloyds + output_tsb + output_monzo
 
 write_output(output)
